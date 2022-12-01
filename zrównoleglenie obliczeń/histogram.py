79f7d54ac85dd49baefed8a48330115728b68bd4
Kasia2005 <116577705+Kasia2005@users.noreply.github.com>
2022-12-01 21:46:33 +0100

    Update histogram.py

--- zrównoleglenie obliczeń/histogram.py
+++ zrównoleglenie obliczeń/histogram.py
@@ -6,8 +6,6 @@ data = []
 for i in range(100):
     data.append(randint(-11, 10))
 
-# print(data)
-# generating a histogram single thread
 min_val = min(data)
 max_val = max(data)
 hist_vals = [0] * (max_val - min_val + 1)
@@ -17,7 +15,6 @@ for d in data:
 
 print("1 thread result:", hist_vals)
 
-# generating a histogram multi threading
 minval = min(data)
 maxval = max(data)
 hist_vals1 = [0] * (maxval - minval + 1) #tablice na histogram
@@ -41,7 +38,6 @@ t2.start()
 t1.join()
 t2.join()
 
-# print("Wątek 1:", hist_vals1,"\nWątek 2:", hist_vals2)
 result_multithread = []
 for i in range(len(hist_vals1)):
     result_multithread.append(hist_vals1[i] + hist_vals2[i])
