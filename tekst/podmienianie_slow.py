a22c7c9ab6c635acd191ea25bea7e1337ee1fe69
Kasia2005 <116577705+Kasia2005@users.noreply.github.com>
2022-12-01 21:31:23 +0100

    Update podmienianie_slow.py

--- tekst/podmienianie_slow.py
+++ tekst/podmienianie_slow.py
@@ -1,15 +1,19 @@
-words_dictionary = {" i ": " oraz ", " oraz ": " i ", " nigdy ": " prawie nigdy ", " dlaczego ": " czemu "}
+def main():    
+    words_dictionary = {" i ": " oraz ", " oraz ": " i ", " nigdy ": " prawie nigdy ", " dlaczego ": " czemu "}
 
-with open("tekst.txt", 'r') as text_file:
-    temp_text = text_file.read()
-
-
-for word in words_dictionary:
-    temp_text = temp_text.replace(word, words_dictionary[word])
-
-with open("replaced_file.txt", 'w') as replaced_file_temp:
-    replaced_file_temp.write(temp_text)
+    with open("tekst.txt", 'r') as text_file:
+        temp_text = text_file.read()
 
 
-text_file.close()
-replaced_file_temp.close()
+    for word in words_dictionary:
+        temp_text = temp_text.replace(word, words_dictionary[word])
+
+    with open("replaced_file.txt", 'w') as replaced_file_temp:
+        replaced_file_temp.write(temp_text)
+
+
+    text_file.close()
+    replaced_file_temp.close()
+
+if __name__ == '__main__':
+    main()
