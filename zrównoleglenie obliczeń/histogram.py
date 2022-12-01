c25b54d8200e0c730e22169067a1c1931dd2782a
Kasia2005 <116577705+Kasia2005@users.noreply.github.com>
2022-12-01 21:45:52 +0100

    Update histogram.py

--- zrównoleglenie obliczeń/histogram.py
+++ zrównoleglenie obliczeń/histogram.py
@@ -1,40 +1,48 @@
+from random import randint
 import threading
 import time
-from random import randint
+
+data = []
+for i in range(100):
+    data.append(randint(-11, 10))
+
+# print(data)
+# generating a histogram single thread
+min_val = min(data)
+max_val = max(data)
+hist_vals = [0] * (max_val - min_val + 1)
+for d in data:
+    hist_vals[d+min_val] += 1
 
 
-histogram = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+print("1 thread result:", hist_vals)
+
+# generating a histogram multi threading
+minval = min(data)
+maxval = max(data)
+hist_vals1 = [0] * (maxval - minval + 1) #tablice na histogram
+hist_vals2 = [0] * (maxval - minval + 1)
 
 
-class myThread(threading.Thread):
-    def __init__(self, threadID, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
+def histogram_thread(input_tab, min, output_tab):
+    for d in input_tab:
+        output_tab[d+min] += 1
 
-    def run(self):
-        print("Starting " + self.name)
-        make_histogram(self.counter, 0.01)
-        print("Exiting " + self.name)
+data_size = len(data)
+part_list1 = data[0:int(data_size/2)]
+part_list2 = data[int(-data_size/2):]
+time.sleep(5)
+t1 = threading.Thread(target=histogram_thread, args=(part_list1, minval, hist_vals1,))
+t2 = threading.Thread(target=histogram_thread, args=(part_list2, minval, hist_vals2,))
 
+t1.start()
+t2.start()
 
-def make_histogram(counter, delay):
-    while counter:
-        time.sleep(delay)
-        num = (randint(0,100))
-        for i in range(1,10):
-            if num <= i*10:
-                histogram[i-1] += 1
-                break
-            else:
-                continue
-        print(histogram)
-        counter -= 1
+t1.join()
+t2.join()
 
-
-thread1 = myThread(1, "Thread-1", 50)
-thread2 = myThread(2, "Thread-2", 50)
-
-thread1.start()
-thread2.start()
+# print("Wątek 1:", hist_vals1,"\nWątek 2:", hist_vals2)
+result_multithread = []
+for i in range(len(hist_vals1)):
+    result_multithread.append(hist_vals1[i] + hist_vals2[i])
+print("2 threads result:", result_multithread)
